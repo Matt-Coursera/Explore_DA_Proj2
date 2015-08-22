bbe9517f5e6b9bb09a70f6d0d4ec4910f93e50ce
Matt Coursera <matt.y.coursera@gmail.com>
2015-08-22 03:55:37 -0400

    Adding plot1.R to GitHUB.

---
+++ plot1.R
@@ -0,0 +1,19 @@
+#Code to plot plot1.png
+
+library(plyr)
+library(ggplot2)
+
+
+#Setup for download
+archiveURL <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip"
+archiveFile <- "NEI_data.zip"
+
+#Download the file
+download.file(url=archiveURL,destfile=archiveFile)
+
+#Uncompress files
+unzip ("NEI_data.zip")
+
+# Reading in the data
+SRC <- readRDS("Source_Classification_Code.rds")
+NEI <- readRDS("summarySCC_PM25.rds")
