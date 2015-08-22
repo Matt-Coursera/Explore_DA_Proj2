62efc94a2e880c717789ce508f7c1e520eff4803
Matt Coursera <matt.y.coursera@gmail.com>
2015-08-22 04:30:16 -0400

    Adding plot1.png and additional code to plot1.R

--- plot1.R
+++ plot1.R
@@ -1,5 +1,7 @@
 #Code to plot plot1.png
 
+#Loading all possible needed libraries
+library(utils)
 library(plyr)
 library(ggplot2)
 
@@ -16,3 +18,21 @@ unzip ("NEI_data.zip")
 # Reading in the data
 SRC <- readRDS("Source_Classification_Code.rds")
 NEI <- readRDS("summarySCC_PM25.rds")
+
+
+#Prepping for plot.
+length(NEI$Emissions)
+length(NEI$year)
+tot.PM25yr <- tapply(NEI$Emissions, NEI$year, sum)
+
+#Building png.
+png("plot1.png")
+
+plot(names(tot.PM25yr), tot.PM25yr, type="l", xlab = "Year", ylab = expression
+     ("Total" ~ PM[2.5] ~"Emissions (tons)"), main = expression("Total US" ~
+        PM[2.5] ~ "Emissions by Year"), col="Purple")
+dev.off()
+#Building plot
+plot(names(tot.PM25yr), tot.PM25yr, type="l", xlab = "Year",
+     ylab = expression ("Total" ~ PM[2.5] ~"Emissions (tons)"), main = expression
+     ("Total US" ~ PM[2.5] ~ "Emissions by Year"), col="Purple")
