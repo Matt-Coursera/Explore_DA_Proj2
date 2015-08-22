2ea19e0cff2c43c23e5763a8b0c2717e8d7b19a3
Matt Coursera <matt.y.coursera@gmail.com>
2015-08-22 03:27:33 -0400

    Added file download, uncompression and reading.

--- plot1.R
+++ plot1.R
@@ -1,4 +1,18 @@
 #Code to plot plt1.png
-readRDS(file, refhook = NULL)   #Sets up to read file
-test <- readRDS(file, refhook = NULL)  #Reads file into variable "test"
-View(test)  # View "test"
+library(plyr)
+library(ggplot2)
+
+
+#Setup for download
+archiveURL <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip"
+archiveFile <- "NEI_data.zip"
+
+#Download the file
+download.file(url=archiveURL,destfile=archiveFile)
+
+#Uncompress files
+unzip ("NEI_data.zip")
+
+# Reading in the data
+SRC <- readRDS("Source_Classification_Code.rds")
+NEI <- readRDS("summarySCC_PM25.rds")
