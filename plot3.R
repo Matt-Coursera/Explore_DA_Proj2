e3627dddd75d6f1990a90b9bed12a5ef7358e07d
Matt Coursera <matt.y.coursera@gmail.com>
2015-08-23 00:52:25 -0400

    Adding code and png files for plot 3 & 5.

--- plot3.R
+++ plot3.R
@@ -1,1 +1,31 @@
 # Code to plot plot3.png
+
+#Loading all possible needed libraries
+library(utils)
+library(plyr)
+library(ggplot2)
+
+# Reading in the data
+SCC <- readRDS("Source_Classification_Code.rds")
+NEI <- readRDS("summarySCC_PM25.rds")
+
+#Building subsets for plot
+balt <- subset (NEI, fips == "24510")
+typePM <- ddply(balt, .(year, type), function(x) sum(x$Emissions))
+
+## Rename the col: Emissions
+colnames(typePM)[3] <- "Emissions"
+
+#Building png.
+png("plot3.png")
+qplot(year, Emissions, data=typePM, color=type, geom ="line") +
+        ggtitle(expression("Baltimore City" ~ PM[2.5] ~
+        "Emmission by source, type and year")) + xlab("Year") +
+        ylab(expression("Total" ~ PM[2.5] ~ "Emissions (in tons)"))
+dev.off()
+
+#Building plot
+qplot(year, Emissions, data=typePM, color=type, geom ="line") +
+        ggtitle(expression("Baltimore City" ~ PM[2.5] ~
+        "Emmission by source, type and year")) + xlab("Year") +
+        ylab(expression("Total" ~ PM[2.5] ~ "Emissions (in tons)"))
--- plot5.R
+++ plot5.R
@@ -1,1 +1,34 @@
 # Code to plot plot5.png
+
+#Loading all possible needed libraries
+library(utils)
+library(plyr)
+library(ggplot2)
+
+# Reading in the data
+SCC <- readRDS("Source_Classification_Code.rds")
+NEI <- readRDS("summarySCC_PM25.rds")
+
+#Building subsets for plot assumpting motor vehicles = On and
+## check the levels for types of vehicles defined
+v.uniq <- unique(grep("Vehicles", SCC$EI.Sector, ignore.case = TRUE, value = TRUE))
+vehic <- SCC[SCC$EI.Sector %in% v.uniq, ]["SCC"]
+
+#Building subsets for the vehicles emissions from Baltimore, MD.
+emV.ba <- NEI[NEI$SCC %in% vehic$SCC & NEI$fips == "24510",]
+
+#Building subset of emissions for vehicles in Baltimore for every year
+baltv.pm <- ddply(emV.ba, .(year), function(x) sum(x$Emissions))
+colnames(baltv.pm)[2] <- "Emissions"
+
+#Building png.
+png("plot5.png")
+qplot(year, Emissions, data=baltv.pm, geom="line") + ggtitle(expression("Baltimore City"
+        ~ PM[2.5] ~ "Motor Vehicle Emissions by Year")) + xlab("Year") + ylab(expression("Total"
+         ~ PM[2.5] ~ "Emissions (tons)"))
+dev.off()
+
+#Building plot
+qplot(year, Emissions, data=baltv.pm, geom="line") + ggtitle(expression("Baltimore City"
+        ~ PM[2.5] ~ "Motor Vehicle Emissions by Year")) + xlab("Year") + ylab(expression("Total"
+        ~ PM[2.5] ~ "Emissions (tons)"))
